c1249b3c155f6c79c3f52f45de35ae6e9a10e6e5
hyojin shin <dsvit100hjin@gmail.com>
2025-03-20 11:16:33 +0900

    read(all)기능 구현

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    # 전체 게시물 읽기
+    articles = Article.objects.all()
+
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'index.html', context)
